6bd0d9917802adbb513161e762af2b6ced65836f
shanerrr <theshanukg@gmail.com>
2024-04-25 12:12:54 -0600

    chore: updated lazy packages

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -23,6 +23,7 @@ return {
 			lspconfig.tsserver.setup({ capabilities = capabilities })
 			lspconfig.lua_ls.setup({ capabilities = capabilities })
 			lspconfig.astro.setup({ capabilities = capabilities })
+
 			vim.keymap.set("n", "<C-Space>", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
 			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
