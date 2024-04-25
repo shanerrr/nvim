a6a6407b548f9b9319b6dc499430c9805e0c7415
shanerrr <theshanukg@gmail.com>
2024-04-24 23:09:58 -0600

    added vim config file

--- init.lua
+++ init.lua
@@ -1,5 +1,3 @@
-vim.g.mapleader = " "
-
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
 	vim.fn.system({
@@ -14,4 +12,5 @@ end
 
 vim.opt.rtp:prepend(lazypath)
 
+require("vim-options")
 require("lazy").setup("plugins")
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -22,6 +22,7 @@ return {
 			local lspconfig = require("lspconfig")
 			lspconfig.tsserver.setup({ capabilities = capabilities })
 			lspconfig.lua_ls.setup({ capabilities = capabilities })
+			lspconfig.astro.setup({ capabilities = capabilities })
 			vim.keymap.set("n", "<C-Space>", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
 			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
